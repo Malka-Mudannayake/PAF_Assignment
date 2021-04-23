69ca3dfec6e547b6d1a78e5e882925569e6b5ce8
Diwyangi Vithana <Diwyangi Vithana@DESKTOP-PAJAHIO>
2021-04-23 12:33:13 +0530

    Add "Insert Order Method"

--- OrderManagementService/src/main/java/com/OrderService.java
+++ OrderManagementService/src/main/java/com/OrderService.java
@@ -1,5 +1,39 @@
 package com;
 
-public class OrderService {
+import model.Order;
 
+import java.util.*;
+
+//For REST Service
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+//For JSON
+import com.google.gson.*;
+//For XML
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
+
+@Path("/Orders")
+public class OrderService {
+	Order orderObj = new Order();
+
+	
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertOrder(@FormParam("productName") String productName, @FormParam("orderQuantity") String orderQuantity,
+			@FormParam("price") String price, @FormParam("prodDesc") String prodDesc ,
+			@FormParam("orderDate") String orderDate ) {
+		String output = orderObj.insertOrder(productName, orderQuantity, price, prodDesc,orderDate);
+		return output;
+	}
+	
+	
+
+
+
+
+	
 }
--- OrderManagementService/src/main/java/model/Order.java
+++ OrderManagementService/src/main/java/model/Order.java
@@ -1,5 +1,61 @@
+
 package model;
 
-public class Order {
+import java.sql.*;
 
+import com.google.gson.JsonElement;
+
+
+public class Order {
+	
+	// A common method to connect to the DB
+	private Connection connect() {
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// Provide the correct details: DBServer/DBName, username, password
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test", "root", "");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+
+	//add order to whishlist
+	public String insertOrder(String productName, String quantity, String price, String desc,String orderDate) {
+		String output = "";
+		try {
+			Connection con = connect();
+			if (con == null) {
+				return "Error while connecting to the database for inserting.";
+			}
+			// create a prepared statement
+			String query = " insert into orders(`orderID`,`productName`,`quantity`,`price`,`prodDesc`,`orderDate`)"
+					+ " values (?, ?, ?, ?, ?, ?)";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			// binding values
+			preparedStmt.setInt(1, 0);
+			preparedStmt.setString(2, productName);
+			preparedStmt.setString(3, quantity);
+			preparedStmt.setDouble(4, Double.parseDouble(price));
+			preparedStmt.setString(5, desc);
+			preparedStmt.setString(6, orderDate);
+			// execute the statement
+
+			preparedStmt.execute();
+			con.close();
+			output = "Inserted successfully";
+		} catch (Exception e) {
+			output = "Error while inserting the order.";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
+	
+	
+
+		
+
+		
 }
