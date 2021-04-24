1679975b58ad6b894f405e80b07ec37b207109dc
Diwyangi Vithana <Diwyangi Vithana@DESKTOP-PAJAHIO>
2021-04-24 19:05:47 +0530

    Formatter on the file and fix indentations

--- OrderManagementService/src/main/java/model/Order.java
+++ OrderManagementService/src/main/java/model/Order.java
@@ -52,7 +52,7 @@ public class Order {
 		return output;
 	}
 
-//read order details 
+	//read order details 
 	public String readOrders() {
 		String output = "";
 		try {
@@ -61,8 +61,9 @@ public class Order {
 				return "Error while connecting to the database for reading.";
 			}
 			// Prepare the html table to be displayed
-			output = "<table border='1'><tr><th> Order ID </th> <th>Product Name</th><th>Price</th>" + "<th>Quantity</th>"
-					+ "<th>Product Description</th>" + "<th>Order Date</th>" + "<th>Update</th><th>Remove</th><th>Buy</th></tr>";
+			output = "<table border='1'><tr><th> Order ID </th> <th>Product Name</th><th>Price</th>"
+					+ "<th>Quantity</th>" + "<th>Product Description</th>" + "<th>Order Date</th>"
+					+ "<th>Update</th><th>Remove</th><th>Buy</th></tr>";
 
 			String query = "select * from orders";
 			Statement stmt = con.createStatement();
@@ -87,7 +88,7 @@ public class Order {
 				output += "<td><input name='btnUpdate' type='button' value='Update'class='btn btn-secondary'></td>"
 						+ "<td><form method='post' action='orders.jsp'>"
 						+ "<input name='btnRemove' type='submit' value='Remove'class='btn btn-danger'></td>"
-						+ "<td><input name='btnBuy' type='submit' value='Buy'class='btn btn-danger'>" 
+						+ "<td><input name='btnBuy' type='submit' value='Buy'class='btn btn-danger'>"
 						+ "<input name='orderID' type='hidden' value='" + orderID + "'>" + "</form></td></tr>";
 			}
 			con.close();
@@ -99,43 +100,42 @@ public class Order {
 		}
 		return output;
 	}
-	
-	//update order details
-	public String updateOrder(String orderID, String productName, String quantity, String price, String prodDesc,String orderDate)
 
-	 {
-			 String output = "";
-			 try
-			 {
-			 Connection con = connect();
-			 if (con == null)
-			 {return "Error while connecting to the database for updating."; }
-			 // create a prepared statement
-			 String query = "UPDATE orders SET productName=?,quantity=?,price=?,prodDesc=?, orderDate=? WHERE orderID=?";
-			 PreparedStatement preparedStmt = con.prepareStatement(query);
-			 // binding values
-			 
-			 preparedStmt.setString(1, productName);
-			 preparedStmt.setString(2, quantity);
-			 preparedStmt.setDouble(3, Double.parseDouble(price));
-			 preparedStmt.setString(4, prodDesc);
-			 preparedStmt.setString(5, orderDate);
-			 preparedStmt.setInt(6, Integer.parseInt(orderID));
-			
-			 // execute the statement
-			 preparedStmt.execute();
-			 con.close();
-			 output = "Updated successfully";
-			 }
-			 catch (Exception e)
-			 {
-			 output = "Error while updating the order.";
-			 System.err.println(e.getMessage());
-			 }
-				return output;
+	// update order details
+	public String updateOrder(String orderID, String productName, String quantity, String price, String prodDesc,
+			String orderDate)
+
+	{
+		String output = "";
+		try {
+			Connection con = connect();
+			if (con == null) {
+				return "Error while connecting to the database for updating.";
 			}
-	
-	//delete order
+			// create a prepared statement
+			String query = "UPDATE orders SET productName=?,quantity=?,price=?,prodDesc=?, orderDate=? WHERE orderID=?";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			// binding values
+
+			preparedStmt.setString(1, productName);
+			preparedStmt.setString(2, quantity);
+			preparedStmt.setDouble(3, Double.parseDouble(price));
+			preparedStmt.setString(4, prodDesc);
+			preparedStmt.setString(5, orderDate);
+			preparedStmt.setInt(6, Integer.parseInt(orderID));
+
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+			output = "Updated successfully";
+		} catch (Exception e) {
+			output = "Error while updating the order.";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
+
+	// delete order
 	public String deleteOrder(String orderID) {
 		String output = "";
 		try {
@@ -159,8 +159,9 @@ public class Order {
 		return output;
 	}
 
-	//insert Payments
-	public String insertPay(String orderID, String payMethod, String cardType, String cardNo, String SSN,String cardExpDate,String amount) {
+	// insert Payments
+	public String insertPay(String orderID, String payMethod, String cardType, String cardNo, String SSN,
+			String cardExpDate, String amount) {
 		String output = "";
 		try {
 			Connection con = connect();
@@ -173,11 +174,11 @@ public class Order {
 			PreparedStatement preparedStmt = con.prepareStatement(query);
 			// binding values
 			preparedStmt.setInt(1, 0);
-			preparedStmt.setInt(2,Integer.parseInt(orderID));
+			preparedStmt.setInt(2, Integer.parseInt(orderID));
 			preparedStmt.setString(3, payMethod);
 			preparedStmt.setString(4, cardType);
 			preparedStmt.setInt(5, Integer.parseInt(cardNo));
-			preparedStmt.setInt(6,Integer.parseInt(SSN));
+			preparedStmt.setInt(6, Integer.parseInt(SSN));
 			preparedStmt.setString(7, cardExpDate);
 			preparedStmt.setString(8, amount);
 			// execute the statement
@@ -191,8 +192,8 @@ public class Order {
 		}
 		return output;
 	}
-	
-	//read order payements
+
+	// read order payements
 	public String readOrderPay() {
 		String output = "";
 		try {
@@ -201,8 +202,9 @@ public class Order {
 				return "Error while connecting to the database for reading.";
 			}
 			// Prepare the html table to be displayed
-			output = "<table border='1'><tr><th> pay ID </th> <th>order ID</th><th>Payment Method</th>" + "<th>Card Type</th>"
-					+ "<th>Card No</th> <th> SSN </th>"  + "<th>Card ExpD Date</th> <th>Amount </th>" + "</tr>";
+			output = "<table border='1'><tr><th> pay ID </th> <th>order ID</th><th>Payment Method</th>"
+					+ "<th>Card Type</th>" + "<th>Card No</th> <th> SSN </th>"
+					+ "<th>Card ExpD Date</th> <th>Amount </th>" + "</tr>";
 
 			String query = "select * from orderpay ";
 			Statement stmt = con.createStatement();
@@ -213,11 +215,11 @@ public class Order {
 				String orderID = Integer.toString(rs.getInt("orderID"));
 				String payMethod = rs.getString("payMethod");
 				String cardType = rs.getString("cardType");
-				String cardNo  = rs.getString("cardNo");
+				String cardNo = rs.getString("cardNo");
 				String SSN = rs.getString("SSN");
 				String cardExpDate = rs.getString("cardExpDate");
 				String amount = rs.getString("amount");
-				
+
 				// Add into the html table
 				output += "<tr><td>" + payID + "</td>";
 				output += "<td>" + orderID + "</td>";
@@ -227,7 +229,7 @@ public class Order {
 				output += "<td>" + SSN + "</td>";
 				output += "<td>" + cardExpDate + "</td>";
 				output += "<td>" + amount + "</td></tr>";
-				
+
 			}
 			con.close();
 			// Complete the html table
